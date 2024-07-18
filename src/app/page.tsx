cad7d723202b3839fd9a73ce323c33c955409ba2
ViniHGV <ixxvinicius@gmail.com>
2024-07-18 12:41:06 -0300

    fix: realizando ajustes na estilização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/Header/Header";
 import SwiperGeneric from "@/components/Swiper/SwiperGeneric";
 import Image from "next/image";
 import sheet from "@/../public/assets/sheet.png";
+import SwiperPartners from "@/components/SwiperPartners/SwiperPartners";
 
 export default function Home() {
   return (
@@ -18,10 +19,16 @@ export default function Home() {
         <SwiperGeneric />
         <BannerSwiper />
         <Image
-          className="absolute right-10 bottom-1/4 rotate-180"
+          className="absolute right-10 bottom-[5%] rotate-[170deg]"
           src={sheet}
           alt="Imagem de ilustração do lado do swiper"
         />
+      </div>
+      <div className="my-32 flex flex-col items-center gap-10 relative mb-32 xl:mb-32">
+        <p className="text-5xl text-center text-blueFontHeader font-semibold">
+          Parceiros
+        </p>
+        <SwiperPartners />
       </div>
       <Footer />
     </div>
