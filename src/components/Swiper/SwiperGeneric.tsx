411b8966f769f00150febefbd458fd313c3638e7
ViniHGV <ixxvinicius@gmail.com>
2024-07-18 12:42:21 -0300

    feat: adicionando estilizações personalizadas pra o swiper

--- src/components/Swiper/SwiperGeneric.tsx
+++ src/components/Swiper/SwiperGeneric.tsx
@@ -3,35 +3,50 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import primaryImageSwiper from "@/../public/assets/PrimaryImageSwiper.png";
+import secondaryImageSwiper from "@/../public/assets/secondaryImageSwiper.png";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/scrollbar";
-import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import "./styles.css";
+import "./stylesSwiper.css";
 
-// import required modules
-import { Scrollbar, Navigation } from "swiper/modules";
+import { Scrollbar, Navigation, Pagination } from "swiper/modules";
 import Image from "next/image";
 
 export default function SwiperGeneric() {
   return (
     <>
       <Swiper
-        scrollbar={{
-          hide: true,
-          enabled: true,
+        style={
+          {
+            "--swiper-pagination-color": "#1C4F9C",
+            "--swiper-pagination-progressbar-size": "100%",
+            "--swiper-pagination-bullet-border-radius": "5%",
+            "--swiper-pagination-bullet-inactive-color": "#FFF",
+            "--swiper-pagination-bullet-inactive-opacity": "1",
+            "--swiper-pagination-bullet-width": "180px",
+            "--swiper-pagination-bullet-height": "4px",
+            "--swiper-pagination-bullet-horizontal-gap": "20px",
+            "--swiper-pagination-bottom": "-2px",
+            "--swiper-navigation-sides-offset": "15px",
+            "--swiper-navigation-color": "#013773",
+            "--swiper-navigation-size": "20px",
+          } as React.CSSProperties
+        }
+        pagination={{
+          clickable: true,
         }}
-        modules={[Scrollbar, Navigation]}
+        spaceBetween={100}
         navigation
-        className="mySwiper"
+        modules={[Scrollbar, Navigation, Scrollbar, Pagination]}
+        className="mySwiperWithBanner"
       >
         <SwiperSlide>
           <Image alt="Imagem do carousel" src={primaryImageSwiper} />
         </SwiperSlide>
         <SwiperSlide>
-          <Image alt="Imagem do carousel" src={primaryImageSwiper} />
+          <Image alt="Imagem do carousel" src={secondaryImageSwiper} />
         </SwiperSlide>
       </Swiper>
     </>
