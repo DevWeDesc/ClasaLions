1eadf9f06cb77f7a19181f5d4511ec05317b9ae9
ViniHGV <ixxvinicius@gmail.com>
2024-07-19 14:42:13 -0300

    fix: ajustando lógica de edição de cores ao clicar um link

--- src/components/Header/NavigationMenuHeader.tsx
+++ src/components/Header/NavigationMenuHeader.tsx
@@ -15,52 +15,69 @@ import {
 } from "@/components/ui/navigation-menu";
 import { FaBuildingColumns, FaCircleQuestion } from "react-icons/fa6";
 import { IoIosHome, IoIosBook } from "react-icons/io";
+import process from "process";
 
 export const NavigationMenuHeader = () => {
-  const [navSelected, setNavSelected] = React.useState(["Home"]);
+  const [navSelect, setNavSelected] = React.useState(
+    window.location.href.substring(
+      process.env.NEXT_PUBLIC_URL_DEV?.length as number
+    )
+  );
+  var currentUrl = window.location.href.substring(
+    process.env.NEXT_PUBLIC_URL_DEV?.length as number
+  );
+  const conditionalStyle = (href: string) => navSelect === href;
 
   return (
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <Link href="/" legacyBehavior passHref>
+          <Link onClick={() => setNavSelected("/")} href="/">
             <NavigationMenuLink
               className={`${navigationMenuTriggerStyle()} ${
-                navSelected.includes("Home") && "text-yellowButton"
+                conditionalStyle("/") && "text-yellowButton"
               }`}
             >
               <div className="flex gap-1 items-center relative">
                 <IoIosHome /> <p> Home</p>
-                {navSelected.includes("Home") && (
+                {conditionalStyle("/") && (
                   <div className="absolute w-4/5 left-1/2 -bottom-6 -translate-x-1/2 h-1 rounded-full bg-yellowButton"></div>
                 )}
               </div>
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
-        <NavigationMenuItem>
+        <NavigationMenuItem
+          className={`${conditionalStyle("/programs") && "text-yellowButton"}`}
+        >
           <NavigationMenuTrigger>
-            <div className="flex items-center gap-1">
+            <div className="flex items-center gap-1 relative">
               <IoIosBook />
-              Programas
+              <p>Programas</p>
+              {conditionalStyle("/programs") && (
+                <div className="absolute w-4/5 left-1/2 -bottom-6 -translate-x-1/2 h-1 rounded-full bg-yellowButton"></div>
+              )}
             </div>
           </NavigationMenuTrigger>
           <NavigationMenuContent className="bg-yellowButton border-b-4 border-b-blueDefault">
             <ul className="flex flex-col w-[400px] gap-1 p-2 md:w-[500px] md:grid-cols-2 lg:w-[570px] text-blueFontHeader">
               <ListItem
-                href="/"
+                onClick={() => setNavSelected("/programs")}
+                href="/programs"
                 className="hover:bg-white/10 cursor-pointer text-blueFontHeader font-semibold"
               >
                 Programa de integração ao mundo do trabalho - Integrar
               </ListItem>
               <ListItem
-                href="/"
+                onClick={() => setNavSelected("/programs")}
+                href="/programs"
                 className="hover:bg-white/10 cursor-pointer text-blueFontHeader font-semibold"
               >
                 Serviço de convivência e fortalecimento de vínculos - SCFV
               </ListItem>
               <ListItem
-                href="/"
+                onClick={() => setNavSelected("/programs")}
+                href="/programs"
                 className="hover:bg-white/10 cursor-pointer text-blueFontHeader font-semibold"
               >
                 Programa de aprendizagem - Socioaprendizagem
@@ -86,13 +103,13 @@ export const NavigationMenuHeader = () => {
           <NavigationMenuContent className=" bg-yellowButton border-b-4 border-b-blueDefault">
             <ul className="flex flex-col w-[400px] gap-1 p-2 md:w-[500px] md:grid-cols-2 lg:w-[570px] text-blueFontHeader">
               <ListItem
-                href="/"
+                href="/programs"
                 className="hover:bg-white/10 cursor-pointer text-blueFontHeader font-semibold"
               >
                 Programa de integração ao mundo do trabalho - Integrar
               </ListItem>
               <ListItem
-                href="/"
+                href="/programs"
                 className="hover:bg-white/10 cursor-pointer text-blueFontHeader font-semibold"
               >
                 Serviço de convivência e fortalecimento de vínculos - SCFV
