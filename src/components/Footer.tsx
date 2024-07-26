aead865f60ae03551c0c169fde8bb0c5af8a2cb5
ViniHGV <ixxvinicius@gmail.com>
2024-07-26 15:25:08 -0300

    feat: adicionando linkd das redes sociais

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,28 @@ import sheetFooter from "@/../public/assets/sheetFooter.png";
 import { FiFacebook, FiInstagram, FiLinkedin } from "react-icons/fi";
 import { FooterSheets } from "./FooterSheets";
 import Image from "next/image";
+import Link from "next/link";
+import { ReactElement } from "react";
+
+interface SocialMedia {
+  icon: ReactElement;
+  href: string;
+}
+
+const SocialMediaImpl: SocialMedia[] = [
+  {
+    href: "https://www.instagram.com/clasa.oficial?igsh=MTR0MWZtN2NwZ2Rjcg==",
+    icon: <FiInstagram className="text-2xl" />,
+  },
+  {
+    href: "https://www.facebook.com/share/UZHDV4EKZe1LPqJ2/?mibextid=qi2Omg",
+    icon: <FiFacebook className="text-2xl" />,
+  },
+  {
+    href: "https://www.linkedin.com/company/clasalions/",
+    icon: <FiLinkedin className="text-2xl" />,
+  },
+];
 
 export const Footer = () => {
   return (
@@ -18,15 +40,17 @@ export const Footer = () => {
       <div className="flex flex-col gap-3">
         <p>Acompanhe nosso trabalho</p>
         <div className="flex justify-center items-center gap-2">
-          <FiInstagram className="text-2xl" />
-          <FiFacebook className="text-2xl" />
-          <FiLinkedin className="text-2xl" />
+          {SocialMediaImpl.map((socialMedia, index) => (
+            <Link target="_blank" key={index} href={socialMedia.href}>
+              {socialMedia.icon}
+            </Link>
+          ))}
         </div>
       </div>
       <Image
         src={sheetFooter}
         className="absolute -top-20 -right-48 rotate-[115deg] bg-blue"
-        alt=""
+        alt="Logo rodapé"
       />
 
       <FooterSheets />
