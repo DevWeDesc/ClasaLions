79a8dc44b982d1db70a981fb7984443f1a5d13ac
ViniHGV <ixxvinicius@gmail.com>
2024-07-26 15:23:42 -0300

    feat: adicionando sessão de sobre nós

--- src/app/institution/page.tsx
+++ src/app/institution/page.tsx
@@ -1,32 +1,18 @@
-import { BannerSwiper } from "@/components/BannerSwiper";
 import { Footer } from "@/components/Footer";
-import SwiperGeneric from "@/components/Swiper/SwiperGeneric";
 import Image from "next/image";
-import sheet from "@/../public/assets/sheet.png";
-import SwiperPartners from "@/components/SwiperPartners/SwiperPartners";
+import imageClasa from "@/../public/assets/ImageClasaSantoAndre.jpg";
+import { About } from "./About";
 
 export default function Institution() {
   return (
     <>
-      <div className="my-32 relative mb-80 xl:mb-96">
+      <div className="my-[92px] relative mb-80 xl:mb-96">
         <Image
-          className="absolute left-10"
-          src={sheet}
+          src={imageClasa}
+          className="object-cover"
           alt="Imagem de ilustração do lado do swiper"
         />
-        <SwiperGeneric />
-        <BannerSwiper />
-        <Image
-          className="absolute right-10 bottom-[5%] rotate-[170deg]"
-          src={sheet}
-          alt="Imagem de ilustração do lado do swiper"
-        />
-      </div>
-      <div className="my-32 flex flex-col items-center gap-10 relative mb-32 xl:mb-32">
-        <p className="text-5xl text-center text-blueFontHeader font-semibold">
-          Parceiros
-        </p>
-        <SwiperPartners />
+        <About />
       </div>
       <Footer />
     </>
