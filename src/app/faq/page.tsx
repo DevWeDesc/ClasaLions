9d6e5fdbcfb39645f7036722938770a1c282b265
ViniHGV <ixxvinicius@gmail.com>
2024-07-30 13:40:16 -0300

    fix: realizando ajustes em textos

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,27 +1,34 @@
 import { Footer } from "@/components/Footer";
 import React from "react";
+import { AccordionGeneric } from "./Accordion";
 
 export default function FAQ() {
   return (
     <>
       <div className="my-32 px-20 flex flex-col gap-32">
-        <div className="grid grid-cols-8 gap-10">
-          <div className="col-span-6">
-            <p className="text-2xl text-blueFontHeader font-bold">
-              FAQ/DÚVIDAS FREQUENTES
+        <div className="flex flex-col gap-16">
+          <div>
+            <p className="text-3xl text-blueFontHeader font-bold">
+              FAQ/DÚVIDAS <br /> FREQUENTES
             </p>
             <p className="text-xl text-blueFontHeader">
-              A CLASA é uma organização da sociedade civil (OSC) sem fins
-              lucrativos que atua há mais de 40 anos na promoção do
-              desenvolvimento de jovens e adolescentes em situação de
+              <strong>A CLASA</strong> é uma organização da sociedade civil{" "}
+              <strong>(OSC) sem fins lucrativos</strong>que atua há mais de{" "}
+              <strong>40 anos</strong> na promoção do desenvolvimento de{" "}
+              <strong>jovens e adolescentes</strong> em situação de
               vulnerabilidade social.
             </p>
-            <p className="text-xl text-blueFontHeader">
-              Através de diversos programas e serviços, oferece acolhimento,
-              orientação, formação profissional e oportunidades de socialização
-              aos jovens, contribuindo para sua inclusão social e construção de
-              um futuro promissor.
+            <p className="text-xl text-blueFontHeader mt-5">
+              Através de diversos <strong>programas e serviços</strong>, oferece
+              acolhimento, orientação,{" "}
+              <strong>formação profissional e oportunidades</strong> de
+              socialização aos <strong>jovens</strong>, contribuindo para sua
+              inclusão social e construção de um{" "}
+              <strong>futuro promissor</strong>.
             </p>
+          </div>
+          <div className="flex flex-col gap-16">
+            <AccordionGeneric />
           </div>
         </div>
       </div>
