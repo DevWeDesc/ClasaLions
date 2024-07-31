62c63d320ec40bed5f7c68f13cca7917fde69920
ViniHGV <ixxvinicius@gmail.com>
2024-07-31 12:17:00 -0300

    feat: criando arrays de imagens para exibição no swiper de parceiros das páginas home e institution

---
+++ src/data/PartnersInstitution.ts
@@ -0,0 +1,12 @@
+import Febraeda from "@/../public/assets/Febraeda logo.png";
+import MesaBrasil from "@/../public/assets/Mesa Brasil logo.png";
+import Feasa from "@/../public/assets/Feasa logo.png";
+import Semeasa from "@/../public/assets/Semasa logo.png";
+import { StaticImageData } from "next/image";
+
+export const PartnersInstitution: string[] | StaticImageData[] = [
+  Febraeda,
+  MesaBrasil,
+  Feasa,
+  Semeasa,
+];
---
+++ src/data/PertnersData.ts
@@ -0,0 +1,14 @@
+import primaryImageSwiper from "@/../public/assets/PrimaryPartner.png";
+import logoCoop from "@/../public/assets/logoCoop.jpg";
+import logoGTI from "@/../public/assets/logoGTI.png";
+import logoClube from "@/../public/assets/logoClube.png";
+import logoWm from "@/../public/assets/logoWm.png";
+import { StaticImageData } from "next/image";
+
+export const PartnersData: string[] | StaticImageData[] = [
+  primaryImageSwiper,
+  logoCoop,
+  logoGTI,
+  logoClube,
+  logoWm,
+];
